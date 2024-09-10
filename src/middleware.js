e879b89c920cb9e5b61c1d95ee3fcd3ef932f030
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-09-10 17:10:26 +0530

    Edge Error

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export const config = {
   matcher: ["/admin/:path*"],
 };
 
-export default async function(req) {
+export default async function middleware(req) {
   // Allow the request to continue
   const session = await getToken({ req: req, secret: process.env.AUTH_SECRET })
   if (!session || !session?.user?.isAdmin) {
